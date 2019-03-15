58f2d1bcf382174edfb728bb68d257385fec67ba
marktwtn <marktwtn@gmail.com>
2019-03-15 20:19:42 +0800

    Add install script for basic tools

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# How to use: $ bash install.sh
+
+sudo apt install \
+  build-essential \
+  cgdb \
+  cmake \
+  fish \
+  gitk \
+  gnuplot \
+  maven \
+  npm \
+  python-pip \
+  tig \
+  tmux \
+  vim \
